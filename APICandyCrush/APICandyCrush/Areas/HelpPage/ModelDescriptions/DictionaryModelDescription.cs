7968dd803a77b2e708bc3f8c40ec0e36e25fa6fa
Kevin Illidan <illidankj@hotmail.com>
2016-08-14 19:32:24 -0600

    api

---
+++ APICandyCrush/APICandyCrush/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace APICandyCrush.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ APICandyCrush/APICandyCrush/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="APICandyCrush.WebApiApplication" Language="C#" %>
