4919461ce57e99814ffbc6d971e5c43c32dce943
Kevin Illidan <illidankj@hotmail.com>
2016-08-14 22:12:58 -0600

    enable cors controller 2

---
+++ APICandyCrush/APICandyCrush/Controllers/UsersController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using APICandyCrush.Models;
+using System.Web.Http.Cors;
+
+namespace APICandyCrush.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/Users
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public IQueryable<User> GetUsers()
+        {
+            return db.Users;
+        }
+
+        // GET: api/Users/5
+        [ResponseType(typeof(User))]
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public IHttpActionResult GetUser(int id)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        // PUT: api/Users/5
+        [ResponseType(typeof(void))]
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public IHttpActionResult PutUser(int id, User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != user.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Users
+        [ResponseType(typeof(User))]
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public IHttpActionResult PostUser(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Users.Add(user);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = user.id }, user);
+        }
+
+        // DELETE: api/Users/5
+        [ResponseType(typeof(User))]
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public IHttpActionResult DeleteUser(int id)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            db.Users.Remove(user);
+            db.SaveChanges();
+
+            return Ok(user);
+        }
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        private bool UserExists(int id)
+        {
+            return db.Users.Count(e => e.id == id) > 0;
+        }
+    }
+}
